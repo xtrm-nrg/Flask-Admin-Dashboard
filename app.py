ec3941c9fff3f5204f2c0d4c90856b5ae8945017
Chris <tsudonymn@gmail.com>
2021-11-05 01:45:49 -0400

    refactor out the initialization of the db

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import flask_admin
 from flask import Flask, url_for, render_template
 from flask_admin import helpers as admin_helpers
 from flask_security import Security, SQLAlchemyUserDatastore
-from flask_security.utils import encrypt_password
 
 # Create Flask application
 from models import db, User, Role, DoIt, DidIt
@@ -53,67 +52,6 @@ def security_context_processor():
         h=admin_helpers,
         get_url=url_for
     )
-
-
-def build_sample_db():
-    """
-    Populate a small db with some example entries.
-    """
-
-    import string
-    import random
-
-    db.drop_all()
-    db.create_all()
-
-    with app.app_context():
-        user_role = Role(name='user')
-        super_user_role = Role(name='superuser')
-        db.session.add(user_role)
-        db.session.add(super_user_role)
-        db.session.commit()
-
-        test_user = user_datastore.create_user(
-            first_name='Admin',
-            email='admin',
-            password=encrypt_password('admin'),
-            roles=[user_role, super_user_role]
-        )
-
-        first_names = [
-            'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie', 'Sophie', 'Mia',
-            'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
-            'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
-        ]
-        last_names = [
-            'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
-            'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
-            'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
-        ]
-
-        for i in range(len(first_names)):
-            tmp_email = first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
-            tmp_pass = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
-            user_datastore.create_user(
-                first_name=first_names[i],
-                last_name=last_names[i],
-                email=tmp_email,
-                password=encrypt_password(tmp_pass),
-                roles=[user_role, ]
-            )
-
-        fish = DoIt(name='Feed Fish', description="Feed the fish")
-        maintenance = DoIt(name='Tank Maintenance', description="Change water, clean filters, vacuum sand")
-        dishes = DoIt(name='Unload dishwasher', description="Unload dishwasher")
-        laundry = DoIt(name='Do laundry', description="Do laundry")
-
-        db.session.add(fish)
-        db.session.add(maintenance)
-        db.session.add(dishes)
-        db.session.add(laundry)
-
-        db.session.commit()
-    return
 
 
 if __name__ == '__main__':
---
+++ db_init.py
@@ -0,0 +1,70 @@
+#!venv/bin/python
+from flask_security.utils import encrypt_password
+
+from app import app, user_datastore
+# Create Flask application
+from models import Role, DoIt
+from models import db
+
+
+def build_sample_db():
+    """
+    Populate a small db with some example entries.
+    """
+
+    import string
+    import random
+
+    db.drop_all()
+    db.create_all()
+
+    with app.app_context():
+        user_role = Role(name='user')
+        super_user_role = Role(name='superuser')
+        db.session.add(user_role)
+        db.session.add(super_user_role)
+        db.session.commit()
+
+        test_user = user_datastore.create_user(
+            first_name='Admin',
+            email='admin',
+            password=encrypt_password('admin'),
+            roles=[user_role, super_user_role]
+        )
+
+        first_names = [
+            'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie', 'Sophie', 'Mia',
+            'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
+            'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
+        ]
+        last_names = [
+            'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
+            'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
+            'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
+        ]
+
+        for i in range(len(first_names)):
+            tmp_email = first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
+            tmp_pass = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
+            user_datastore.create_user(
+                first_name=first_names[i],
+                last_name=last_names[i],
+                email=tmp_email,
+                password=encrypt_password(tmp_pass),
+                roles=[user_role, ]
+            )
+
+        fish = DoIt(name='Feed Fish', description="Feed the fish")
+        maintenance = DoIt(name='Tank Maintenance', description="Change water, clean filters, vacuum sand")
+        dishes = DoIt(name='Unload dishwasher', description="Unload dishwasher")
+        laundry = DoIt(name='Do laundry', description="Do laundry")
+
+        db.session.add(fish)
+        db.session.add(maintenance)
+        db.session.add(dishes)
+        db.session.add(laundry)
+
+        db.session.commit()
+    return
+
+
